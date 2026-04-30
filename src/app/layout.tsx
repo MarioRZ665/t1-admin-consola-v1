5a44fc41b3994c16d25eacbcf1442504543b2094
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2026-04-30 15:30:31 -0600

    refactor: migrate project from Vite to Next.js framework

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter, Outfit } from 'next/font/google';
 import './globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import ThemeRegistry from '@/components/ThemeRegistry';
 
 const inter = Inter({
